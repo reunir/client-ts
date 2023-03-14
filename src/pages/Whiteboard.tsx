7db66b27183592c8571115ec450954af93eae8c6
Rhythm Shandlya <armaanbgp@gmail.com>
2023-03-15 00:05:33 +0530

    onchange

--- src/pages/Whiteboard.tsx
+++ src/pages/Whiteboard.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { WhiteboardWithEditor } from "../components/whiteboard";
+import { WhiteboardWithEditor } from "../components/whiteboard-new";
 
 type Props = {};
 
 const Whiteboard = (props: Props) => {
+  const handleOnChange = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <WhiteboardWithEditor />
+      <WhiteboardWithEditor onChange={handleOnChange} />
     </div>
   );
 };
